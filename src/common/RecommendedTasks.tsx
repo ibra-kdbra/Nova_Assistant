9f1cdb513dcc4fbe3405f9cb4a4d9b74ee744d3e
ibra-kdbra <git-repo-develop@outlook.com>
2024-07-05 21:30:10 +0300

    first recommended tasks examples

---
+++ src/common/RecommendedTasks.tsx
@@ -0,0 +1,63 @@
+import { Button, VStack, Text } from "@chakra-ui/react";
+import { useAppState } from "../state/store";
+
+const tasks = [
+  'Post on twitter.com with content "An automated post from ibra-kdbra by @ibra-kdbra! :)" If I\'m not logged in, fail the task and wait for me to log in.',
+  "Find a book about AI and add a physical copy to cart on Amazon.com. Pick the cheaper one from paperback and hardcover.",
+];
+
+const RecommendedTasks = ({
+  runTask,
+}: {
+  runTask: (instructions: string) => void;
+}) => {
+  const state = useAppState((state) => ({
+    instructions: state.ui.instructions,
+  }));
+  if (state.instructions) {
+    return null;
+  }
+
+  const onButtonClick = (idx: number) => {
+    runTask(tasks[idx]);
+  };
+
+  return (
+    <VStack spacing={2} align="stretch">
+      <Text fontSize="large" mt={1}>
+        Examples:
+      </Text>
+      <Button
+        textAlign="left"
+        display="block"
+        variant="outline"
+        height="4rem"
+        onClick={() => onButtonClick(0)}
+      >
+        <Text fontWeight={600} noOfLines={1}>
+          Post on twitter.com
+        </Text>
+        <Text fontWeight={400} noOfLines={1} color="gray">
+          with content &quot;An automated post from ibra-kdbra by
+          @ibra-kdbra!&quot;
+        </Text>
+      </Button>
+      <Button
+        textAlign="left"
+        display="block"
+        variant="outline"
+        height="4rem"
+        onClick={() => onButtonClick(1)}
+      >
+        <Text fontWeight={600} noOfLines={1}>
+          Find a book about AI
+        </Text>
+        <Text fontWeight={400} noOfLines={1} color="gray">
+          and add a physical copy to cart on Amazon.com
+        </Text>
+      </Button>
+    </VStack>
+  );
+};
+
+export default RecommendedTasks;

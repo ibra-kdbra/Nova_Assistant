38fbc4835b8e0eb4c98de60e9e6cc7c02b22c6ca
ibra-kdbra <git-repo-develop@outlook.com>
2024-07-30 21:19:10 +0300

    test: none

--- utils/reload/initReloadServer.js
+++ utils/reload/initReloadServer.js
@@ -23,6 +23,14 @@ function debounce(callback, delay) {
         timer = setTimeout(() => callback(...args), delay);
     };
 }
+/*
+This debounce function allows you to control the execution frequency of a function.
+Whenever the inner function is called within the specified delay window,
+it resets the timer and ensures the callback is called only after the entire delay has passed without any further calls.
+This is useful for scenarios where you want to avoid overwhelming the system with too many function calls in rapid succession.
+For example, debouncing can be used to optimize event handlers triggered by user input (like typing in a search bar)
+or image resize events during window resizing.
+*/
 
 const clientsThatNeedToUpdate = new Set();
 let needToForceReload = false;
